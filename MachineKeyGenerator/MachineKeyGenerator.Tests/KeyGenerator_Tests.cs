60fcfe19cf0fec6f2fd489a1a3b8d221b2849e76
Tieson Trowbridge <tiesont@gmail.com>
2015-05-02 03:17:02 -0400

    Added simple test project
    Added unit test for 1M iterations; no duplicate keys seem to be
    generated by KeyGenerator.

---
+++ MachineKeyGenerator/MachineKeyGenerator.Tests/KeyGenerator_Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using FluentAssertions;
+
+namespace MachineKeyGenerator.Tests
+{
+    [TestClass]
+    public class KeyGenerator_Tests
+    {
+        int validationKeySize = 64;
+        int decryptionKeySize = 32;
+
+
+        [TestMethod]
+        public void Generate_ValidationKey_Should_Not_Duplicate_Keys()
+        {
+            int iterations = 1000000;
+
+            var generator = new MachineKeyGenerator.Web.KeyGenerator();
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (!set.Add(generator.GenerateKey(validationKeySize)))
+                {
+                    break;
+                }
+            }
+
+            set.Count.ShouldBeEquivalentTo<int>(iterations, "RNGCryptoServiceProvider is fairly random, and should generate the specified number of random keys without duplicates.");
+        }
+
+        [TestMethod]
+        public void Generate_DecryptionKey_Should_Not_Duplicate_Keys()
+        {
+            int iterations = 1000000;
+
+            var generator = new MachineKeyGenerator.Web.KeyGenerator();
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (!set.Add(generator.GenerateKey(decryptionKeySize)))
+                {
+                    break;
+                }
+            }
+
+            set.Count.ShouldBeEquivalentTo<int>(iterations, "RNGCryptoServiceProvider is fairly random, and should generate the specified number of random keys without duplicates.");
+        }
+
+        [TestMethod]
+        public void Generate_BothKeys_Should_Not_Duplicate_Keys()
+        {
+            int iterations = 1000000;
+
+            var generator = new MachineKeyGenerator.Web.KeyGenerator();
+            HashSet<string> validationSet = new HashSet<string>();
+            HashSet<string> decryptionSet = new HashSet<string>();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                if (!validationSet.Add(generator.GenerateKey(validationKeySize)))
+                {
+                    break;
+                }
+
+                if (!decryptionSet.Add(generator.GenerateKey(decryptionKeySize)))
+                {
+                    break;
+                }
+            }
+
+            validationSet.Count.ShouldBeEquivalentTo<int>(iterations, "RNGCryptoServiceProvider is fairly random, and should generate the specified number of validation keys without duplicates.");
+
+            decryptionSet.Count.ShouldBeEquivalentTo<int>(iterations, "RNGCryptoServiceProvider is fairly random, and should generate the specified number of decryption keys without duplicates.");
+        }
+    }
+}
