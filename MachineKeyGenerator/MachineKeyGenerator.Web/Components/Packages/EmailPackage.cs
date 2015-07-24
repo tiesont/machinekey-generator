8da30222902a490c13a2af4d40e6eacd706239e0
Tieson Trowbridge <tiesont@gmail.com>
2015-07-23 23:55:39 -0400

    Adding packages for email and logging config.

---
+++ MachineKeyGenerator/MachineKeyGenerator.Web/Components/Packages/EmailPackage.cs
@@ -0,0 +1,15 @@
+﻿using Postal;
+
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace MachineKeyGenerator.Web
+{
+    public class EmailPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IEmailService, EmailServiceAdapter>();
+        }
+    }
+}
---
+++ MachineKeyGenerator/MachineKeyGenerator.Web/Components/Packages/LoggingPackage.cs
@@ -0,0 +1,13 @@
+﻿using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace MachineKeyGenerator.Web
+{
+    public class LoggingPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<ILogger, ElmahLogger>();
+        }
+    }
+}
